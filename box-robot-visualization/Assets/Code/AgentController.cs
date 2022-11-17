2c10a9f0ce77f2e3d47ec310d28526ee305e5441
Paulo Ogando Gulias <A01751587@itesm.mx>
2022-11-17 13:19:37 -0600

    Fixed Errors

--- box-robot-visualization/Assets/Code/AgentController.cs
+++ box-robot-visualization/Assets/Code/AgentController.cs
@@ -39,14 +39,14 @@ public class CajaData : AgentData
 {
     public bool recogido;
 
-    public CajaData(string id, float x, float y, float z, bool has_box) : base(id, x, y, z)
+    public CajaData(string id, float x, float y, float z, bool recogido) : base(id, x, y, z)
     {
         this.recogido = recogido;
     }
 }
 
 [Serializable]
-public class EstanteData
+public class EstanteData : AgentData
 {
     public int boxes;
 
@@ -71,7 +71,7 @@ public class CajasData
 {
     public List<CajaData> positions;
 
-    public CajasData() => this.positions = new List<RobotData>();
+    public CajasData() => this.positions = new List<CajaData>();
 }
 
 [Serializable]
@@ -104,8 +104,8 @@ public class AgentController : MonoBehaviour
     RobotsData robotsData;
     CajasData cajasData;
     EstantesData estanteData;
-    Dictionary<string, GameObject> robots, paredes, cajas, puertas, estantes;
-    Dictionary<string, Vector3> prevPositions, currPositions;
+    public Dictionary<string, GameObject> agents, robotsCaja;
+    public Dictionary<string, Vector3> prevPositions, currPositions;
 
     bool updated = false, started = false;
 
@@ -116,16 +116,17 @@ public class AgentController : MonoBehaviour
 
     void Start()
     {
-        agentsData = new AgentsData();
-        obstacleData = new AgentsData();
+        robotsData = new RobotsData();
+        cajasData = new CajasData();
         puertasData = new AgentsData();
         paredesData = new AgentsData();
-        estantesData = new AgentsData();
+        estanteData = new EstantesData();
 
         prevPositions = new Dictionary<string, Vector3>();
         currPositions = new Dictionary<string, Vector3>();
 
         agents = new Dictionary<string, GameObject>();
+        robotsCaja = new Dictionary<string, GameObject>();
 
         floor.transform.localScale = new Vector3((float)width/10, 1, (float)height/10);
         floor.transform.localPosition = new Vector3((float)width/2-0.5f, 0, (float)height/2-0.5f);
@@ -156,9 +157,16 @@ public class AgentController : MonoBehaviour
 
                 Vector3 interpolated = Vector3.Lerp(previousPosition, currentPosition, dt);
                 Vector3 direction = currentPosition - interpolated;
-
-                agents[agent.Key].transform.localPosition = interpolated;
-                if(direction != Vector3.zero) agents[agent.Key].transform.rotation = Quaternion.LookRotation(direction);
+                
+                if(robotsCaja[agent.Key].activeInHierarchy) {
+                    robotsCaja[agent.Key].transform.localPosition = interpolated;
+                    if(direction != Vector3.zero) robotsCaja[agent.Key].transform.rotation = Quaternion.LookRotation(direction);
+                }
+                else {
+                    agents[agent.Key].transform.localPosition = interpolated;
+                    if(direction != Vector3.zero) agents[agent.Key].transform.rotation = Quaternion.LookRotation(direction);
+                }
+                
             }
 
             // float t = (timer / timeToUpdate);
@@ -175,7 +183,7 @@ public class AgentController : MonoBehaviour
             Debug.Log(www.error);
         else 
         {
-            StartCoroutine(GetAgentsData());
+            StartCoroutine(GetRobotsData());
             StartCoroutine(GetCajasData());
         }
     }
@@ -219,23 +227,34 @@ public class AgentController : MonoBehaviour
             Debug.Log(www.error);
         else 
         {
-            agentsData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
+            robotsData = JsonUtility.FromJson<RobotsData>(www.downloadHandler.text);
 
-            foreach(AgentData agent in agentsData.positions)
+            foreach(RobotData robot in robotsData.positions)
             {
-                Vector3 newAgentPosition = new Vector3(agent.x, agent.y, agent.z);
+                Vector3 newRobotPosition = new Vector3(robot.x, robot.y, robot.z);
 
                     if(!started)
                     {
-                        prevPositions[agent.id] = newAgentPosition;
-                        agents[agent.id] = Instantiate(agentPrefab, newAgentPosition, Quaternion.identity);
+                        prevPositions[robot.id] = newRobotPosition;
+                            agents[robot.id] = Instantiate(robotPrefab, newRobotPosition, Quaternion.identity);
+                            robotsCaja[robot.id] = Instantiate(robotboxPrefab, newRobotPosition, Quaternion.identity);
+                            robotsCaja[robot.id].SetActive(false);
                     }
                     else
                     {
                         Vector3 currentPosition = new Vector3();
-                        if(currPositions.TryGetValue(agent.id, out currentPosition))
-                            prevPositions[agent.id] = currentPosition;
-                        currPositions[agent.id] = newAgentPosition;
+                        if(currPositions.TryGetValue(robot.id, out currentPosition))
+                            prevPositions[robot.id] = currentPosition;
+                        currPositions[robot.id] = newRobotPosition;
+                        if(robot.has_box)
+                        {   
+                            agents[robot.id].SetActive(false);
+                            robotsCaja[robot.id].SetActive(true);
+                        }
+                        else {
+                            agents[robot.id].SetActive(true);
+                            robotsCaja[robot.id].SetActive(false);
+                        }
                     }
             }
 
@@ -253,9 +272,9 @@ public class AgentController : MonoBehaviour
             Debug.Log(www.error);
         else 
         {
-            agentsData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
+            cajasData = JsonUtility.FromJson<CajasData>(www.downloadHandler.text);
 
-            foreach(AgentData agent in agentsData.positions)
+            foreach(CajaData agent in cajasData.positions)
             {
                 Vector3 newAgentPosition = new Vector3(agent.x, agent.y, agent.z);
 
@@ -270,6 +289,8 @@ public class AgentController : MonoBehaviour
                         if(currPositions.TryGetValue(agent.id, out currentPosition))
                             prevPositions[agent.id] = currentPosition;
                         currPositions[agent.id] = newAgentPosition;
+                        if(agent.recogido) agents[agent.id].SetActive(false);
+                        else agents[agent.id].SetActive(true);
                     }
             }
 
@@ -287,13 +308,13 @@ public class AgentController : MonoBehaviour
             Debug.Log(www.error);
         else 
         {
-            obstacleData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
+            estanteData = JsonUtility.FromJson<EstantesData>(www.downloadHandler.text);
 
-            Debug.Log(obstacleData.positions);
+            Debug.Log(estanteData.positions);
 
-            foreach(AgentData obstacle in obstacleData.positions)
+            foreach(EstanteData estante in estanteData.positions)
             {
-                Instantiate(estantePrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
+                Instantiate(estantePrefab, new Vector3(estante.x, estante.y, estante.z), Quaternion.identity);
             }
         }
     }
@@ -307,11 +328,9 @@ public class AgentController : MonoBehaviour
             Debug.Log(www.error);
         else 
         {
-            obstacleData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
+            paredesData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
 
-            Debug.Log(obstacleData.positions);
-
-            foreach(AgentData obstacle in obstacleData.positions)
+            foreach(AgentData obstacle in paredesData.positions)
             {
                 Instantiate(paredesPrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
             }
@@ -327,13 +346,11 @@ public class AgentController : MonoBehaviour
             Debug.Log(www.error);
         else 
         {
-            obstacleData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
+            puertasData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
 
-            Debug.Log(obstacleData.positions);
-
-            foreach(AgentData obstacle in obstacleData.positions)
+            foreach(AgentData puerta in puertasData.positions)
             {
-                Instantiate(puertaPrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
+                Instantiate(puertaPrefab, new Vector3(puerta.x, puerta.y, puerta.z), Quaternion.identity);
             }
         }
     }
